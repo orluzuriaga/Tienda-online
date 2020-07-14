5629a91a3e6f7f33550ad49a8f5bdcd1399da58d
renato-luzuriaga <renatoluzuriaga@gmail.com>
2020-07-14 10:40:42 +0200

    Creo el formulario reactivo de productos

--- src/app/products/product-add/product-add.component.ts
+++ src/app/products/product-add/product-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'ed-product-add',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAddComponent implements OnInit {
 
+
+  form: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    brand: new FormControl(''),
+    price: new FormControl(''),
+    saleprice: new FormControl(''),
+    thumImage: new FormControl(''),
+  })
   constructor() { }
 
   ngOnInit(): void {
