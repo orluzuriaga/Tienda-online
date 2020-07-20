a0e494006c4f6520c1250a86ec145ced985b726d
renato_luzuriaga <renatoluzuriaga@gmail.com>
2020-07-20 10:32:57 +0200

    Añado los servicios de la tienda

--- src/app/products/shared/services/products.service.ts
+++ src/app/products/shared/services/products.service.ts
@@ -32,9 +32,12 @@ export class ProductsService {
     return this.httpClient.put<Product>(`${PRODUCTO_URL}/${product.id}`, product);
 
   }
-
+// 
 
   delete(id:string):Observable<Product>{
     return this.httpClient.delete<Product>(`${PRODUCTO_URL}/${id}`)
   }
+
+
+
 }
